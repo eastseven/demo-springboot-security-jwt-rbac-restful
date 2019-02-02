f01c9fd4c8df100938e2836a0d3f49480a97e26b
eastseven <eastseven@foxmail.com>
2019-02-02 16:52:35 +0800

    提交一把

--- src/main/java/cn/eastseven/Application.java
+++ src/main/java/cn/eastseven/Application.java
@@ -1,18 +1,67 @@
 package cn.eastseven;
 
+import cn.eastseven.security.RoleEntity;
+import cn.eastseven.security.RoleRepository;
+import cn.eastseven.security.UserEntity;
+import cn.eastseven.security.UserRepository;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author d7
  * 入口
  */
+@Slf4j
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Autowired
+    private ApplicationContext ctx;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        ctx.getBean(RoleRepository.class).deleteAll();
+        long roleCount = ctx.getBean(RoleRepository.class).count();
+        if (roleCount == 0L) {
+            // user, admin
+            List<RoleEntity> roles = Lists.newArrayList("ROLE_USER", "ROLE_ADMIN")
+                    .stream()
+                    .map(RoleEntity::new)
+                    .collect(Collectors.toList());
+
+            ctx.getBean(RoleRepository.class).saveAll(roles);
+        }
+
+        ctx.getBean(UserRepository.class).deleteAll();
+        long userCount = ctx.getBean(UserRepository.class).count();
+        if (userCount == 0L) {
+            roleCount = ctx.getBean(RoleRepository.class).count();
+            if (roleCount > 0) {
+                BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+                ctx.getBean(RoleRepository.class).findAll().forEach(role -> {
+                    String username = role.getName().toLowerCase().replaceFirst("role_", "");
+                    UserEntity user = new UserEntity(username, passwordEncoder.encode("123456"));
+                    user.getRoles().add(role);
+                    ctx.getBean(UserRepository.class).save(user);
+                    log.debug(">>> {}", user);
+                });
+            }
+        }
+    }
 }
 
--- src/main/java/cn/eastseven/security/SecurityConfig.java
+++ src/main/java/cn/eastseven/security/SecurityConfig.java
@@ -1,16 +1,23 @@
 package cn.eastseven.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author d7
@@ -21,31 +28,107 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] SWAGGER2 = {
-            "/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**"
+            "/v2/api-docs", "/swagger-resources/**/**", "/swagger-ui.html", "/webjars/**/**/**", "/csrf", "/"
     };
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+    @Autowired
+    private JwtSecurityInterceptor securityInterceptor;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Autowired
+    private JwtUserDetailsService jwtUserDetailsService;
+
+    @Autowired
+    private JwtAuthorizationTokenFilter authenticationTokenFilter;
+
+    @Value("${jwt.header}")
+    private String tokenHeader;
+
+    @Value("${jwt.route.authentication.path}")
+    private String authenticationPath;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoderBean());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // 忽略Swagger2页面内容
-        http.authorizeRequests().mvcMatchers(SWAGGER2).permitAll();
-        http.authorizeRequests().mvcMatchers("/users/**").authenticated();
+    @Bean
+    public PasswordEncoder passwordEncoderBean() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
-        return new InMemoryUserDetailsManager(user);
+    @Bean
+    public AccessDecisionManager accessDecisionManager() {
+        return new JwtAccessDecisionManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // we don't need CSRF because our token is invulnerable
+                .csrf().disable()
+
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+
+                // don't create session
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+
+                .authorizeRequests()
+
+                // Un-secure H2 Database
+                .antMatchers("/h2-console/**/**").permitAll()
+                .antMatchers(SWAGGER2).permitAll()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated();
+
+        httpSecurity
+                .addFilterBefore(authenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+        httpSecurity
+                .addFilterBefore(securityInterceptor, FilterSecurityInterceptor.class);
+
+        // disable page caching
+        httpSecurity
+                .headers()
+                .frameOptions().sameOrigin()  // required to set for H2 else H2 Console will be blank.
+                .cacheControl();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // AuthenticationTokenFilter will ignore the below paths
+        web
+                .ignoring()
+                .antMatchers(
+                        HttpMethod.POST,
+                        authenticationPath
+                )
+
+                // allow anonymous resource requests
+                .and()
+                .ignoring()
+                .antMatchers(
+                        HttpMethod.GET,
+                        "/",
+                        "/*.html",
+                        "/favicon.ico",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js"
+                )
+
+                // Un-secure H2 Database (for testing purposes, H2 console shouldn't be unprotected in production)
+                .and()
+                .ignoring()
+                .antMatchers(SWAGGER2)
+                .antMatchers("/h2-console/**/**");
     }
 }
